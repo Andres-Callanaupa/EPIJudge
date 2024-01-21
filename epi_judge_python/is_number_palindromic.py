f441fdb569d1928638ec14c047800ace36c8e20d
Andres-Callanaupa <34245377+Andres-Callanaupa@users.noreply.github.com>
2024-01-21 18:42:41 -0500

    complete 4.9

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,10 +1,17 @@
 from test_framework import generic_test
 
+def reverse_number(x: int) -> int:
+    num = 0
+    while x > 0:
+        num_ones_place = x % 10
+        num = (num * 10) + num_ones_place
+        x //= 10
+    return num
 
 def is_palindrome_number(x: int) -> bool:
-    # TODO - you fill in here.
-    return True
-
+    if x < 0:
+        return False
+    return x == reverse_number(x)
 
 if __name__ == '__main__':
     exit(
